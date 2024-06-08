921b7f916878d45c3531a9a5a476f91914097c6a
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-06-08 14:07:29 +0000

    refactor: validate dto for user registration

--- src/main/java/dev/thesarfo/lexchange/model/dto/request/user/UserRegisterRequest.java
+++ src/main/java/dev/thesarfo/lexchange/model/dto/request/user/UserRegisterRequest.java
@@ -2,18 +2,22 @@ package dev.thesarfo.lexchange.model.dto.request.user;
 
 import dev.thesarfo.lexchange.model.error.ErrorMessages;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record UserRegisterRequest(
+        @NotBlank
         @Email(message = ErrorMessages.VALID_EMAIL_ADDRESS)
         @Size(min = 7, message = ErrorMessages.MINIMUM_EMAIL_LENGTH)
         String email,
 
 
+        @NotBlank
         @Size(min = 7, message = ErrorMessages.MINIMUM_USERNAME_LENGTH)
         String username,
 
-        @Size(min = 8)
+        @NotBlank
+        @Size(min = 8, message = ErrorMessages.MINIMUM_PASSWORD_LENGTH)
         String password
 ) {
 }
