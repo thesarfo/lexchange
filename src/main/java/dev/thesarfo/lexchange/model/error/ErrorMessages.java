ed9f0dcef7c889212f48a718b9bc07bb4a5d2c8e
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-06-08 14:07:56 +0000

    feat: add more success and error messages

--- src/main/java/dev/thesarfo/lexchange/model/error/ErrorMessages.java
+++ src/main/java/dev/thesarfo/lexchange/model/error/ErrorMessages.java
@@ -5,8 +5,10 @@ public class ErrorMessages {
     private ErrorMessages(){}
 
     public static final String USER_NOT_FOUND = "User not found with username: ";
-    public static final String USER_EMAIL_ALREADY_EXISTS = "User already exists with email: ";
     public static final String VALID_EMAIL_ADDRESS = "Please enter valid e-mail address";
     public static final String MINIMUM_EMAIL_LENGTH = "Minimum e-mail length is 7 characters.";
     public static final String MINIMUM_USERNAME_LENGTH = "Minimum username length is 7 characters.";
+    public static final String USER_ALREADY_EXISTS = "User already exists with this username or email";
+    public static final String INVALID_CREDENTIALS = "Invalid credentials";
+    public static final String MINIMUM_PASSWORD_LENGTH = "Minimum password length is 8 characters.";
 }
--- src/main/java/dev/thesarfo/lexchange/model/success/SuccessMessages.java
+++ src/main/java/dev/thesarfo/lexchange/model/success/SuccessMessages.java
@@ -5,4 +5,5 @@ public class SuccessMessages {
     private SuccessMessages(){}
 
     public static final String REGISTRATION_SUCCESSFUL = "Registration successful";
+    public static final String LOGIN_SUCCESSFUL = "Login successful";
 }
