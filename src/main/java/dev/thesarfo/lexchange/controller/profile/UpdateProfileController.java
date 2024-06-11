c286e1eb47cdd81fb7bb8c7753c43c789c5e5710
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-06-11 19:02:54 +0000

    feat: add endpoint to update profile details

---
+++ src/main/java/dev/thesarfo/lexchange/controller/profile/UpdateProfileController.java
@@ -0,0 +1,29 @@
+package dev.thesarfo.lexchange.controller.profile;
+
+import dev.thesarfo.lexchange.model.dto.request.UpdateProfileRequest;
+import dev.thesarfo.lexchange.model.success.SuccessMessages;
+import dev.thesarfo.lexchange.service.profile.ProfileServiceImpl;
+import dev.thesarfo.lexchange.util.ResponseHandler;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/profile")
+public class UpdateProfileController {
+
+    private final ProfileServiceImpl profileService;
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<Object> updateProfile(@PathVariable Long id, @Valid @RequestBody UpdateProfileRequest request){
+        return ResponseHandler.successResponse(SuccessMessages.PROFILE_UPDATED, profileService.updateProfile(id, request),
+                HttpStatus.OK);
+    }
+}
