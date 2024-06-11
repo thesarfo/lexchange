91bb6c065d038cca82bff629756e9972f7998c43
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-06-11 17:01:56 +0000

    implement jps repository for userprofiles

--- src/main/java/dev/thesarfo/lexchange/repository/user/UserProfileRepository.java
+++ src/main/java/dev/thesarfo/lexchange/repository/user/UserProfileRepository.java
@@ -4,4 +4,5 @@ import dev.thesarfo.lexchange.entity.user.UserProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserProfileRepository extends JpaRepository<UserProfile, Long> {
+    UserProfile findByUsername(String username);
 }
