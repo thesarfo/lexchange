5a8a4d9e49c7fff8ce21d643cf4593eb6e6c48b2
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-06-11 21:34:37 +0000

    add methods for validating password changes

--- src/main/java/dev/thesarfo/lexchange/util/GeneralUtil.java
+++ src/main/java/dev/thesarfo/lexchange/util/GeneralUtil.java
@@ -1,11 +1,21 @@
 package dev.thesarfo.lexchange.util;
 
+import dev.thesarfo.lexchange.entity.user.User;
 import dev.thesarfo.lexchange.entity.user.UserProfile;
-import dev.thesarfo.lexchange.model.dto.request.UpdateProfileRequest;
+import dev.thesarfo.lexchange.model.dto.request.profile.UpdateProfileRequest;
+import dev.thesarfo.lexchange.model.dto.request.user.ChangePasswordRequest;
+import dev.thesarfo.lexchange.model.error.ErrorMessages;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
 
+@Component
 public class GeneralUtil {
 
-    private GeneralUtil(){
+    private final PasswordEncoder passwordEncoder;
+
+    private GeneralUtil(PasswordEncoder passwordEncoder){
+        this.passwordEncoder = passwordEncoder;
     }
 
     public static void updateProfileFields(UpdateProfileRequest profile, UserProfile existingProfile) {
@@ -20,8 +30,26 @@ public class GeneralUtil {
         if (profile.bio() != null){
             existingProfile.setBio(profile.bio());
         }
-        if (profile.profilePhoto() != null){
-            existingProfile.setProfilePhoto(profile.profilePhoto());
+//        if (profile.profilePhoto() != null){
+//            existingProfile.setProfilePhoto(profile.profilePhoto());
+//        }
+    }
+
+    public void checkIfIncomingNewPasswordEqualsExistingPassword(ChangePasswordRequest request, User user) {
+        if (request.newPassword().equals(user.getPassword())){
+            throw new BadCredentialsException(ErrorMessages.NEW_PASSWORD_SAME_AS_OLD);
+        }
+    }
+
+    public void checkIfIncomingOldPasswordIsNotEqualToTheExistingPassword(ChangePasswordRequest request, User user) {
+        if (!passwordEncoder.matches(request.oldPassword(), user.getPassword())){
+            throw new BadCredentialsException(ErrorMessages.OLD_PASSWORD_INCORRECT);
+        }
+    }
+
+    public void checkIfIncomingOldPasswordIsEqualToIncomingNewPassword(ChangePasswordRequest request){
+        if (request.oldPassword().equals(request.newPassword())){
+            throw new BadCredentialsException(ErrorMessages.OLD_AND_NEW_PASSWORD_EQUAL);
         }
     }
 }
