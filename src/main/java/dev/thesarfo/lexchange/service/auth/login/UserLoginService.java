c8cdffb1876e0ddc2952993e22e5e84d98cbf0f5
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-06-11 17:02:54 +0000

    remove unused imports

--- src/main/java/dev/thesarfo/lexchange/service/auth/login/UserLoginService.java
+++ src/main/java/dev/thesarfo/lexchange/service/auth/login/UserLoginService.java
@@ -2,7 +2,6 @@ package dev.thesarfo.lexchange.service.auth.login;
 
 import dev.thesarfo.lexchange.model.dto.request.user.UserLoginRequest;
 import dev.thesarfo.lexchange.model.dto.response.user.JwtResponse;
-import org.springframework.http.ResponseEntity;
 
 public interface UserLoginService {
     JwtResponse signin(UserLoginRequest userLoginRequest);
