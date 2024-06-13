d1a066921696586ee93a365a5e0ea04a59499553
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-06-13 10:58:25 +0000

    feat: users can update their profile images

--- src/main/java/dev/thesarfo/lexchange/controller/profile/UpdateProfileController.java
+++ src/main/java/dev/thesarfo/lexchange/controller/profile/UpdateProfileController.java
@@ -10,9 +10,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequiredArgsConstructor
@@ -25,5 +28,11 @@ public class UpdateProfileController {
     public ResponseEntity<Object> updateProfile(@PathVariable Long id, @Valid @RequestBody UpdateProfileRequest request){
         return ResponseHandler.response(SuccessMessages.PROFILE_UPDATED,
                 HttpStatus.OK, profileService.updateProfile(id, request));
+    }
+
+    @PatchMapping("/image/{id}")
+    public ResponseEntity<Object> uploadProfileImage(@PathVariable Long id, @RequestParam("file")MultipartFile file){
+        return ResponseHandler.response(SuccessMessages.PFP_UPDATE_SUCCESS,
+                 HttpStatus.OK, profileService.uploadPfp(id, file.getOriginalFilename(), file));
     }
 }
